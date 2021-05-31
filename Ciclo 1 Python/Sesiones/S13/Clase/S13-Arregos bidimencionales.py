0609249e9e7cd7010ca29d0bfca01d5946d69488
Emanuel Rico Conde <ricocondee@gmail.com>
2021-05-30 19:04:46 -0500

    faltaba algo de la clase

--- Ciclo 1 Python/Sesiones/S13/Clase/S13-Arregos bidimencionales.py
+++ Ciclo 1 Python/Sesiones/S13/Clase/S13-Arregos bidimencionales.py
@@ -28,3 +28,25 @@ for i in range(len(matriz)):
     for j in range(len(matriz[i])):
         print(matriz[i][j],end=" ")
     print()
+
+#suma de matrices
+
+matriza = []
+matrizb = []
+matrizc = []
+
+n = int(input("Filas: "))
+m = int(input("Columnas: "))
+
+for i in range(n):
+    matriza.append([])
+    matrizb.append([])
+    matrizc.append([])
+    for j in range(m):
+        matriza[i].append(int(input("A {} {}: ".format(i+1,j+1))))
+        matrizb[i].append(int(input("B {} {}: ".format(i+1,j+1))))
+        matrizc[i].append(matriza[i][j]+ matrizb[i][j])
+
+print("A: ",matriza)
+print("B: ",matrizb)
+print("La matriz resultante: ",matrizc)
