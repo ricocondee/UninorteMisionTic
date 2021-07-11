874472a36138315de6446f3e595e5ff1280a6de3
Emanuel Rico Conde <ricocondee@gmail.com>
2021-07-11 15:19:55 -0500

    Correccion de una variable

--- Ciclo2Java/Sesiones/S2/Clase/src/Vectores.java
+++ Ciclo2Java/Sesiones/S2/Clase/src/Vectores.java
@@ -8,21 +8,21 @@ public class Vectores {
         int N, i; // Variable para almacenar el número de elementos del array
         System.out.println("Digite la cantidad de edades a ingresar");
         N = input.nextInt(); // Capturar el valor ingresado por teclado
-        int Edades[] = new int[N]; // Definición del array
+        int edades[] = new int[N]; // Definición del array
         float promedio = 0; // Variable para calcular el promedio
 
         // tomar los datos del array
-        System.out.println("-----------Ingreso de Edades----------------");
+        System.out.println("-----------Ingreso de edades----------------");
         for (i = 0; i < N; i++) {
             System.out.println("Digite la edad " + (i + 1));
-            Edades[i] = input.nextInt();
+            edades[i] = input.nextInt();
         }
 
         // Imprimir los datos del array
-        System.out.println("-----------Impresión de Edades----------------");
+        System.out.println("-----------Impresión de edades----------------");
         for (i = 0; i < N; i++) {
-            System.out.println("Edad " + (i + 1) + " = " + Edades[i]);
-            promedio = promedio + Edades[i]; // Sumando todas las edades
+            System.out.println("Edad " + (i + 1) + " = " + edades[i]);
+            promedio = promedio + edades[i]; // Sumando todas las edades
         }
         promedio = promedio / N;
         System.out.printf("El promedio de las edades es: %.2f", promedio);
