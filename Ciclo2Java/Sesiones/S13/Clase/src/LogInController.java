156f6e2aadd18f8d83e5763fea30eb035815acc8
Emanuel Rico Conde <ricocondee@gmail.com>
2021-08-05 20:19:56 -0500

    Sesion 13 05 de agosto, JavaFx

---
+++ Ciclo2Java/Sesiones/S13/Clase/src/LogInController.java
@@ -0,0 +1,18 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LogInController {
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private Button loginButton;
+
+
+}
