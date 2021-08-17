0fd710c41aeae75fc1329105e0dea2cee745c765
Emanuel Rico Conde <ricocondee@gmail.com>
2021-08-17 11:53:24 -0500

    RENAMED

--- Ciclo2Java/Sesiones/S14/Clase/src/Calculadora.java
+++ Ciclo2Java/Sesiones/S14/Clase/src/Calculadora.java
@@ -4,7 +4,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class App extends Application {
+public class Calculadora extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         // FXMLLoader fxmlLoader = new
